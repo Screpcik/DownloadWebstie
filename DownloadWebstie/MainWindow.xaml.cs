bec25f23b121c552991d11afd683e4bfea6eb3c2
screpcik <screpcik12@gmail.com>
2023-02-01 11:50:16 +0100

    Add project files.

---
+++ DownloadWebstie/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DownloadWebstie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUrl = WebsiteUrl.Text;
+                
+            await Task.Run(async () =>
+            {           
+                var webClient = new WebClient();
+
+                var downloadedString = await webClient.DownloadStringTaskAsync(currentUrl);
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    DisplayText.Text = "Sukces";
+                });
+            });
+        }
+
+        private void DoSomeWork()
+        {
+            Thread.Sleep(2222);
+        }
+    }
+}
